41adc677bbaa213cb0eb00f9f4dec96a7e56dab4
thanh tung <tungt3922gmail.com>
2023-01-04 23:01:09 +0700

    code base

---
+++ src/layouts/dashboard/data/reportsLineChartData.js
@@ -0,0 +1,6 @@
+export default {
+  tasks: {
+    labels: ["Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+    datasets: { label: "Desktop apps", data: [50, 40, 300, 220, 500, 250, 400, 230, 500] },
+  },
+};
